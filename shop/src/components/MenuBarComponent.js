68478e939551e2e8bed2949970beb1d1b4db7e12
Roxana-Dogaru <elenaroxanadogaru@gmail.com>
2023-08-30 23:53:40 +0300

    updates for mobile on Menu

--- shop/src/components/MenuBarComponent.js
+++ shop/src/components/MenuBarComponent.js
@@ -1,5 +1,9 @@
 import React, { useEffect, useState, } from "react";
 import { Link } from "react-router-dom";
+import shirt from "../assets/img/shirt.png";
+import dress from "../assets/img/dress.png";
+import jewelry from "../assets/img/jewelry.png";
+import electronics from "../assets/img/electronics.png";
 
 function MenuBarComponent() {
     
@@ -21,15 +25,43 @@ function MenuBarComponent() {
   
     return (
       <>
+        <div className="d-flex justify-content-between d-block d-md-none mx-3">
+        <div className="d-flex"  style={{ width: "60px", height: "60px" }} >
+          <Link to={`/category/${"electronics"}`} >
+            <img src={electronics} className="img-fluid "  alt="electronics" />
+          </Link>
+        </div>
+        <div className=""  style={{ width: "60px", height: "60px" }} >
+          <Link to={`/category/${"jewelery"}`}>
+            <img src={jewelry} className="img-fluid "  alt="jewelry" />
+          </Link>
+        </div>
+        <div className=""  style={{ width: "60px", height: "60px" }} >
+          <Link to={`/category/${"women's clothing"}`}>
+            <img src={dress} className="img-fluid "  alt="dress" />
+          </Link>
+          </div>
+          <div className=""  style={{ width: "60px", height: "60px" }} >
+          <Link to={`/category/${"men's clothing"}`}>
+            <img src={shirt} className="img-fluid "  alt="shirt" />
+          </Link>
+          </div>
+        </div> 
         <div className="d-flex justify-content-around mt-3">
         
         {categories.map(category =>(
            <Link to={`/category/${category}`}>
-          <button  className="btn btn-light" key={categories.item}> {category}</button>
+            <div className=" d-none d-md-block">
+              <button button  className="btn btn-light " key={categories.item}> {category}</button>
+            </div>
+            
+          
           </Link>
-        ))}         
+        ))}  
+        
+      </div>      
                  
-        </div>
+        
         
       </>
       
